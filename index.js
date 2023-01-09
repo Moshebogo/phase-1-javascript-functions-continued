5b010e69d719689b58821aa6cd4db5ee33346d15
Eli Moshe <moshebogo@gmail.com>
2023-01-09 17:05:14 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork (action = "go to the office") {
+ return `This Monday, I will ${action}.`;
+}
+
+function  wrapAdjective(visualFlair = "%") {
+    return function (adjective = "special") {
+        return`You are ${visualFlair}${adjective}${visualFlair}!`;
+    };
+}
+wrapAdjective('*')('a hard worker!');
+wrapAdjective("||")("a dedicated programmer!");
